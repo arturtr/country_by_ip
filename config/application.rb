bfc24bfe76b07ab9f3cdba04fe125f7a89b5a2ab
Artur Trofimov <arturtr@gmail.com>
2016-01-28 15:35:20 +1000

    Typo fix

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CountyByIp
+module CountryByIp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -26,5 +26,7 @@ module CountyByIp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
